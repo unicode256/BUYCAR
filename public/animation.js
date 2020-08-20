bc1d179972b57899d59bb9d2425cad8eb41c1d6f
Timofey <thetimm8890@gmail.com>
2020-08-20 19:30:58 +0300

    created sidebar for mobile with burger

--- public/animation.js
+++ public/animation.js
@@ -2,3 +2,7 @@ $(window).on('scroll', () => {
     let navbar = $('.navbar')[0];
     navbar.classList.toggle('sticky_navbar', window.scrollY > 0);
 });
+
+$('.sidebar_switcher').on('click', () => {
+    $('.menu_toggle')[0].classList.toggle('active');
+})
