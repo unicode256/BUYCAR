7a59791048bbd820665c4df3465d1071510d82d2
Gleb <thetimm8890@gmail.com>
2020-08-22 17:53:39 +0300

    preloader is added

--- public/animation.js
+++ public/animation.js
@@ -1,3 +1,9 @@
+$(window).on('load', () => {
+    setTimeout(() => {
+        $('.preloader')[0].classList.toggle('loaded');
+    }, 1000);
+});
+
 $(window).on('scroll', () => {
     let navbar = $('.navbar')[0];
     navbar.classList.toggle('sticky_navbar', window.scrollY > 0);
