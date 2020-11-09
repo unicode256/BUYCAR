b01dfd5209453e7de282f640986238dfa4309959
Gleb <thetimm8890@gmail.com>
2020-11-09 18:05:03 +0300

    some links added

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
-})
+});
+
+app.get('/ad', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/ad.html'));
+});
+
+app.get('/cars', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/cars.html'));
+});
 
 app.listen(port, () => {
     console.log(`Express web app available at localhost: ${port}`);
