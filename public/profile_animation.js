f20d85d0f92723a7cc24d328691f1998966689d5
Gleb <thetimm8890@gmail.com>
2021-01-16 20:16:15 +0300

    ads page for desktop is created

---
+++ public/profile_animation.js
@@ -0,0 +1,74 @@
+var nav = $('.tabs_controls');
+var line = $('<div />').addClass('line');
+
+line.appendTo(nav);
+
+var active = nav.find('.active');
+var pos = 0;
+var wid = 0;
+
+if(active.length) {
+  pos = active.position().left;
+  wid = active.width();
+  line.css({
+    left: pos,
+    width: wid
+  });
+}
+
+nav.find('ul li a').click(function(e) {
+  e.preventDefault();
+  if(!$(this).parent().hasClass('active') && !nav.hasClass('animate')) {
+    
+    nav.addClass('animate');
+
+    var _this = $(this);
+
+    nav.find('ul li').removeClass('active');
+
+    var li_index = nav.find('ul li a').index(this) + 1;
+    var class_to_delete;
+
+    nav[0].classList.forEach(item => {
+        if(/\d+/.test(item)){
+            class_to_delete = item;
+        }
+    });
+
+    nav.find('ul li').removeClass('active');
+    nav.removeClass(class_to_delete);
+    nav.addClass('tab_' + li_index + '_active');
+
+    var position = _this.parent().position();
+    var width = _this.parent().width();
+
+    if(position.left >= pos) {
+      line.animate({
+        width: ((position.left - pos) + width)
+      }, 300, function() {
+        line.animate({
+          width: width,
+          left: position.left
+        }, 150, function() {
+          nav.removeClass('animate');
+        });
+        _this.parent().addClass('active');
+      });
+    } else {
+      line.animate({
+        left: position.left,
+        width: ((pos - position.left) + wid)
+      }, 300, function() {
+        line.animate({
+          width: width
+        }, 150, function() {
+          nav.removeClass('animate');
+        });
+        _this.parent().addClass('active');
+      });
+    }
+
+    pos = position.left;
+    wid = width;
+  }
+});
